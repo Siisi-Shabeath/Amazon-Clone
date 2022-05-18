c5571a91d0c8e95c9eb036336429db94eac34a13
Jehoshabeath Appiah <94073669+Siisi-Shabeath@users.noreply.github.com>
2022-05-18 23:04:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Checkout from './Checkout';
+
+
+function App() {
+  return (
+    //BEM
+    <Router>
+     <div className="app">
+       <Routes>
+        <Route path="/Checkout" element={[<Header/>, <Checkout />]}/>
+        <Route path="/" element={[<Header />,<Home />]}/>
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCyTQVxesaArric_tr0XWQW-5_jv58y358",
+    authDomain: "challenge-62838.firebaseapp.com",
+    projectId: "challenge-62838",
+    storageBucket: "challenge-62838.appspot.com",
+    messagingSenderId: "385523773260",
+    appId: "1:385523773260:web:094b30ce667fe2bb4229e9",
+    measurementId: "G-VNNMEH23QN"
+  };
